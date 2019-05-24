bb1f787642dc7c47479ff655cff2ba3da36194f0
simon3000 <simon3000@163.com>
2019-05-24 20:01:38 +0800

    Refactor?

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 const fs = require('fs').promises
 
 const io = require('socket.io-client')
-const socket = io('http://0.0.0.0:9001')
-const get = (e, target) => new Promise(resolve => socket.emit(e, target, resolve))
+const socket = io('https://api.vtbs.moe')
 
-const biliAPI = require('bili-api')
 const LiveWS = require('bilibili-live-ws')
-
-const race = (...args) => new Promise((resolve, reject) => {
-  setTimeout(reject, 1000 * 5)
-  biliAPI(...args)
-    .then(resolve)
-})
 
 const wait = ms => new Promise(resolve => setTimeout(resolve, ms))
 
@@ -22,45 +14,59 @@ const openRoom = ({ roomid, speakers = {}, currentFilename = undefined }) => new
   rooms[roomid] = ws
   let lastTime = ''
   let lastHeartbeat = 0
+  let autorestart = setTimeout(() => {
+    console.log(`AUTORESTART: ${roomid}`)
+    ws.close()
+    resolve({ roomid, speakers, currentFilename })
+  }, 1000 * 60 * 60 * 18)
+  let timeout = setTimeout(() => {
+    if (new Date().getTime() - lastHeartbeat > 1000 * 30) {
+      console.log(`TIMEOUT: ${roomid}`)
+      ws.close()
+      clearTimeout(autorestart)
+      clearTimeout(timeout)
+      resolve({ roomid, speakers, currentFilename })
+    }
+  }, 1000 * 45)
   // let storm = []
   ws.once('live', () => {
     console.log(`READY: ${roomid}`)
-    ws.on('DANMU_MSG', async ({ info }) => {
-      if (!info[0][9]) {
-        let message = info[1]
-        if (!message.includes('TIME') || !message.includes('ONLINE')) {
-          let mid = info[2][0]
-          let uname = info[2][1]
-          let date = new Date()
-          let filename = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}.txt`
-          let time = `${date.getHours()}:${date.getMinutes()}`
-          if (!currentFilename) {
-            currentFilename = filename
-          }
-          if (currentFilename !== filename) {
-            let speakerNum = Object.keys(speakers).length
-            let lastFIleName = currentFilename
-            currentFilename = filename
-            if (speakerNum) {
-              let allSpeaker = Object.keys(speakers)
-                .map(key => `${key}:${speakers[key].uname}:${speakers[key].count}`)
-                .join(',')
-              speakers = {}
-              await fs.appendFile(`${roomid}/${lastFIleName}`, `SPEAKERNUM${speakerNum};${allSpeaker}\nV1\n`)
-            }
-          }
-          if (!speakers[mid]) {
-            speakers[mid] = { count: 0, uname }
-          }
-          speakers[mid].count++
-          if (lastTime !== time) {
-            lastTime = time
-            await fs.appendFile(`${roomid}/${filename}`, `TIME${lastTime}ONLINE${ws.online}\n`)
-          }
-          await fs.appendFile(`${roomid}/${filename}`, `${mid}:${message}\n`)
+  })
+  ws.on('DANMU_MSG', async ({ info }) => {
+    if (!info[0][9]) {
+      let message = info[1]
+      if (!message.includes('TIME') || !message.includes('ONLINE')) {
+        let mid = info[2][0]
+        let uname = info[2][1]
+        let date = new Date()
+        let filename = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}.txt`
+        let time = `${date.getHours()}:${date.getMinutes()}`
+        if (!currentFilename) {
+          currentFilename = filename
         }
+        if (currentFilename !== filename) {
+          let speakerNum = Object.keys(speakers).length
+          let lastFIleName = currentFilename
+          currentFilename = filename
+          if (speakerNum) {
+            let allSpeaker = Object.keys(speakers)
+              .map(key => `${key}:${speakers[key].uname}:${speakers[key].count}`)
+              .join(',')
+            speakers = {}
+            await fs.appendFile(`${roomid}/${lastFIleName}`, `SPEAKERNUM${speakerNum};${allSpeaker}\nV1\n`)
+          }
+        }
+        if (!speakers[mid]) {
+          speakers[mid] = { count: 0, uname }
+        }
+        speakers[mid].count++
+        if (lastTime !== time) {
+          lastTime = time
+          await fs.appendFile(`${roomid}/${filename}`, `TIME${lastTime}ONLINE${ws.online}\n`)
+        }
+        await fs.appendFile(`${roomid}/${filename}`, `${mid}:${message}\n`)
       }
-    })
+    }
   })
   ws.on('heartbeat', async () => {
     let date = new Date()
@@ -92,85 +98,53 @@ const openRoom = ({ roomid, speakers = {}, currentFilename = undefined }) => new
       }
     }
   })
-  let autorestart = setTimeout(() => {
-    console.log(`AUTORESTART: ${roomid}`)
-    ws.close()
-    resolve({ roomid, speakers, currentFilename })
-  }, 1000 * 60 * 60 * 18)
   ws.on('heartbeat', () => {
     lastHeartbeat = new Date().getTime()
-    setTimeout(() => {
+    timeout = setTimeout(() => {
       if (new Date().getTime() - lastHeartbeat > 1000 * 30) {
         console.log(`TIMEOUT: ${roomid}`)
         ws.close()
         clearTimeout(autorestart)
+        clearTimeout(timeout)
         resolve({ roomid, speakers, currentFilename })
       }
     }, 1000 * 45)
   })
-  ws.once('close', async () => {
+  ws.on('close', async () => {
     console.log(`CLOSE: ${roomid}`)
     clearTimeout(autorestart)
+    clearTimeout(timeout)
     resolve({ roomid, speakers, currentFilename })
   })
   ws.on('error', async () => {
     console.log(`ERROR: ${roomid}`)
     ws.close()
     clearTimeout(autorestart)
+    clearTimeout(timeout)
     resolve({ roomid, speakers, currentFilename })
   })
 })
 
-const open = async roomid => {
-  let object = await openRoom({ roomid })
+const watch = async roomid => {
+  let object = { roomid }
   for (;;) {
-    await wait(500)
-    if (!rooms[roomid]) {
-      console.log(`DISABLE: ${roomid}`)
-      return
-    }
-    console.log(`REOPEN: ${roomid}`)
     object = await openRoom(object)
+    await wait(250)
+    console.log(`REOPEN: ${roomid}`)
   }
 }
 
-;
-(async () => {
-  for (;;) {
-    let folders = await fs.readdir('.')
-    let vtbs = await get('vtbs')
-    let roomsEnable = []
-    for (let i = 0; i < vtbs.length; i++) {
-      let { mid } = vtbs[i]
-      let object = await race({ mid }, ['roomid'], { wait: 1000 }).catch(() => undefined)
-      if (!object) {
-        i--
-        console.log(`RETRY: ${mid}`)
-        await wait(1000 * 5)
-        continue
-      }
-      if (object.roomid) {
-        let roomid = object.roomid
-        roomsEnable.push(roomid)
-        if (!rooms[roomid]) {
-          if (!folders.includes(String(roomid))) {
-            await fs.mkdir(String(roomid))
-          }
-          console.log(`OPEN: ${i + 1}/${vtbs.length} - ${mid} - ${roomid}`)
-          open(roomid)
+socket.on('info', async info => {
+  let folders = await fs.readdir('.')
+  info.map(({ roomid }) => roomid)
+    .filter(roomid => roomid)
+    .forEach(async roomid => {
+      if (!rooms[roomid]) {
+        rooms[roomid] = true
+        if (!folders.includes(String(roomid))) {
+          await fs.mkdir(String(roomid))
         }
+        watch(roomid)
       }
-    }
-    let roomsOpen = Object.keys(rooms)
-    for (let i = 0; i < roomsOpen.length; i++) {
-      if (rooms[roomsOpen[i]]) {
-        if (!roomsEnable.includes(Number(roomsOpen[i]))) {
-          rooms[roomsOpen[i]].close()
-          rooms[roomsOpen[i]] = false
-        }
-      }
-    }
-    console.log('REFRESH')
-    await wait(1000 * 60 * 60)
-  }
-})()
+    })
+})
