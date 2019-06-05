1dde986bb2016bcf8465a6cbcac456b2a8b64bcb
simon3000 <simon3000@163.com>
2019-06-06 00:57:18 +0800

    internal json api socket

--- monster.js
+++ monster.js
@@ -1,6 +1,9 @@
 const http = require('http')
 const fs = require('fs').promises
 
+const Server = require('socket.io')
+const LRU = require('lru-cache')
+
 http.createServer(async (request, response) => {
   if (!Number.isNaN(Number(request.url.split('/')[1]))) {
     fs.readFile(`.${request.url}`)
@@ -18,12 +21,56 @@ http.createServer(async (request, response) => {
   }
 }).listen(8012)
 
-// const fs = require('fs').promises
+module.exports = api => {
+  const io = new Server(9002, { serveClient: false })
+  let cache = new LRU({ max: 1000, maxAge: 1000 * 15 })
+  io.on('connection', socket => {
+    socket.on('rooms', async (data, arc) => {
+      if (typeof arc === 'function') {
+        let hit = cache.get('rooms')
+        if (!hit) {
+          hit = await api.rooms()
+          cache.set('rooms', hit)
+        }
+        arc(hit)
+      }
+    })
+    socket.on('records', async (data, arc) => {
+      if (typeof arc === 'function') {
+        let key = `records_${data[0]}`
+        let hit = cache.get(key)
+        if (!hit) {
+          hit = await api.records(data[0])
+          cache.set(key, hit)
+        }
+        arc(hit)
+      }
+    })
+    socket.on('roomsRecords', async (data, arc) => {
+      if (typeof arc === 'function') {
+        let hit = cache.get('roomsRecords')
+        if (!hit) {
+          hit = await api.roomsRecords()
+          cache.set('roomsRecords', hit)
+        }
+        arc(hit)
+      }
+    })
+    socket.on('read', async (data, arc) => {
+      if (typeof arc === 'function') {
+        let key = `read_${data[0]}_${data[1]}_${data[2]}`
+        let hit = cache.get(key)
+        if (!hit) {
+          hit = await api.read(data[0], data[1], data[2])
+          cache.set(key, hit)
+        }
+        arc(hit)
+      }
+    })
+  })
+}
 
 // const nodejieba = require('nodejieba')
-
-// const Server = require('socket.io')
-// const io = new Server(9002, { serveClient: false, path: '/' })
 
 // nodejieba.load({
 //   userDict: 'dictionary/userdict.txt',
