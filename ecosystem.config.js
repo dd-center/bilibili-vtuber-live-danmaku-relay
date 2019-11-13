227b2844f64ad733af8567904d46ce31e41de07e
simon3000 <simon30002021@icloud.com>
2019-11-13 19:43:19 +0100

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   apps: [{
-    name: 'vd',
+    name: 'vd-relay',
     script: 'index.js',
     instances: 1,
     autorestart: true,
-    watch: false,
-  }],
+    watch: false
+  }]
 }
